5210d8a3fd9006f496ce22fada79a3d3dd895840
hirfan875 <hirfan875@gmail.com>
2021-02-16 20:14:28 +0500

    Iniatialize address crud

---
+++ database/migrations/2021_02_16_150746_create_addresses_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('name_en',150);
+            $table->string('name_ar',150);
+            $table->string('area',100);
+            $table->string('block',100);
+            $table->string('street',100);
+            $table->string('building',50);
+            $table->string('floor',50);
+            $table->string('civil_id',50);
+            $table->string('other');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
