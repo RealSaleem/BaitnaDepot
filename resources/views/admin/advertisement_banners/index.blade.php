c72fda8c500840f3de78dd732f65673de6e8c931
hirfan875 <hirfan875@gmail.com>
2021-02-19 15:32:52 +0500

    worked on listing delete functionality

--- resources/views/admin/advertisement_banners/index.blade.php
+++ resources/views/admin/advertisement_banners/index.blade.php
@@ -34,45 +34,38 @@
                                     </thead>
                                     <tbody>
                                         @if(sizeof($advertisements) > 0)
-                                            @foreach($advertisements as $advertisement)
-                                            <tr>
-                                                <td>{{$advertisement->id}}</td>
-                                                <td>{{$advertisement->title_en}}</td>
-                                                <td>{{$advertisement->title_ar}}</td>
-                                                <td>
-                                                    @if($advertisement->image != null)
-                                                        <img src="{{$advertisement->image}}" style="width:100px;">
-                                                    @else
-                                                        <img src="{{asset('app-assets/images/no-image.png')}}" style="width:100px">
-                                                    @endif
-                                                </td>
-                                                <td>{{$advertisement->sort}}</td>
-{{--                                                <td>--}}
-{{--                                                    <a href="{{ route('admin.advertisements.edit',$advertisement->id) }}" class="waves-effect waves-light btn btn-small mb-2">{{__('site.edit')}}</a>--}}
-{{--                                                    <form action="{{route('admin.advertisements.destroy', $advertisement->id)}}" method="POST" class="delete-record">--}}
-{{--                                                        @method('DELETE')--}}
-{{--                                                        @csrf--}}
-{{--                                                        <button class="waves-effect waves-light red accent-2 btn btn-small mb-2">{{__('site.delete')}}</button>--}}
-{{--                                                    </form>--}}
-{{--                                                </td>--}}
-
-
-                                                <td style="text-align: center;" >
-                                                    <!-- Dropdown Trigger -->
-                                                    <a class='dropdown-trigger' href='#' data-target='dropdown1'><i class="Small material-icons" style="font-size: 30px;">list</i></a>
-                                                    <ul id='dropdown1' class='dropdown-content' style="width: 200px;">
-                                                        <li><a href="{{ route('admin.advertisements.edit',$advertisement->id) }}"><i class="Small material-icons" style="font-size: 30px;">edit</i> Edit</a></li>
-                                                        <li> <a href="{{route('admin.advertisements.destroy', $advertisement->id)}}" type="submit" class="delete-record"><i class=" material-icons" style="font-size: 30px;">delete_forever</i> Delete</a></li>
-                                                    </ul>
-                                                </td>
-
-
-
-
-                                            </tr>
-                                            @endforeach
+                                        @foreach($advertisements as $advertisement)
+                                        <tr>
+                                            <td>{{$advertisement->id}}</td>
+                                            <td>{{$advertisement->title_en}}</td>
+                                            <td>{{$advertisement->title_ar}}</td>
+                                            <td>
+                                                @if($advertisement->image != null)
+                                                <img src="{{$advertisement->image}}" style="width:100px;">
+                                                @else
+                                                <img src="{{asset('app-assets/images/no-image.png')}}" style="width:100px">
+                                                @endif
+                                            </td>
+                                            <td>{{$advertisement->sort}}</td>
+                                            <td style="text-align: center;">
+                                                <!-- Dropdown Trigger -->
+                                                <a class="dropdown-trigger" href="#" data-target='dropdown1'><i class="Small material-icons" style="font-size: 30px;">list</i></a>
+                                                <ul id="dropdown1" class="dropdown-content">
+                                                    <form action="{{route('admin.advertisements.destroy', $advertisement->id)}}" method="POST" id="delete-row-{{$advertisement->id}}" class="hide">
+                                                        @method('DELETE')
+                                                        @csrf
+                                                        <button class="waves-effect waves-light red accent-2 btn btn-small mb-2">{{__('site.delete')}}</button>
+                                                    </form>
+                                                    <li><a href="{{ route('admin.advertisements.edit',$advertisement->id) }}"><i class="Small material-icons">edit</i> {{__('site.edit')}}</a></li>
+                                                    <li>
+                                                        <a href="javascript:;" data-value="{{$advertisement->id}}" type="submit" class="delete-record"><i class=" material-icons">delete_forever</i> {{__('site.delete')}}</a>
+                                                    </li>
+                                                </ul>
+                                            </td>
+                                        </tr>
+                                        @endforeach
                                         @else
-                                            <td colspan="4">{{ __('site.no_record_found') }}</td>
+                                        <td colspan="4">{{ __('site.no_record_found') }}</td>
                                         @endif
                                     </tbody>
                                     <tfoot>
@@ -98,14 +91,14 @@
 @section('scripts')
 {{-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> --}}
 <script>
-    $(function () {
+    $(function() {
         $('#page-length-option').DataTable({
             // "responsive": true,
             // "lengthMenu": [
             //   [10, 25, 50, -1],
             //   [10, 25, 50, "All"]
             // ]
-            'scrollX':true
+            'scrollX': true
         });
     });
 </script>
@@ -113,43 +106,45 @@
     // function confirm_delete(){
     $('.delete-record').click(function(e) {
         e.preventDefault();
-
+        var rowId = $(this).attr('data-value');
+        console.log(rowId);
         swal({
-            // title: "{!! trans('toaster.confirm_delete') !!}",
-            text: "{!! trans('toaster.confirm_delete') !!}",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-            buttons: {
-                cancel: {
-                text: "{!! trans('site.cancel') !!}",
-                value: false,
-                visible: true,
-                className: ""
-              },
-              confirm: {
-                text: "{!! trans('site.ok') !!}",
-                value: true,
-                visible: true,
-                className: ""
-              }
-            },
-            closeOnClickOutside: false
-        })
-        .then((isConfirm) => {
-            console.log(isConfirm);
-          if (isConfirm) {
-            this.submit();
-            return true;
-            // swal("Poof! Your imaginary file has been deleted!", {
-            //   icon: "success",
-            // });
-          } else {
-            return false;
-            // swal("Your imaginary file is safe!");
-          }
-        });
-    // }
+                // title: "{!! trans('toaster.confirm_delete') !!}",
+                text: "{!! trans('toaster.confirm_delete') !!}",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+                buttons: {
+                    cancel: {
+                        text: "{!! trans('site.cancel') !!}",
+                        value: false,
+                        visible: true,
+                        className: ""
+                    },
+                    confirm: {
+                        text: "{!! trans('site.ok') !!}",
+                        value: true,
+                        visible: true,
+                        className: ""
+                    }
+                },
+                closeOnClickOutside: false
+            })
+            .then((isConfirm) => {
+                console.log(isConfirm);
+                if (isConfirm) {
+                    // this.submit();
+                    $('#delete-row-' + rowId).submit();
+                    return true;
+                    // swal("Poof! Your imaginary file has been deleted!", {
+                    //   icon: "success",
+                    // });
+                } else {
+                    return false;
+                    // swal("Your imaginary file is safe!");
+                }
+            });
+        // }
     });
 </script>
 @endsection
