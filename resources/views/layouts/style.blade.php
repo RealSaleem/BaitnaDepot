e2f2d0913528752141d3c2f4298d11219c5f06f4
hirfan875 <hirfan875@gmail.com>
2021-03-01 14:15:35 +0500

    changes on style.css file

--- resources/views/layouts/style.blade.php
+++ resources/views/layouts/style.blade.php
@@ -1,7 +1,4 @@
 <style>
-
-
-
     #dropdown1 {
         width: 180px !important;
         color: dimgray !important;
@@ -19,23 +16,27 @@
     .dropdown-trigger i {
         color: dimgray !important;
     }
-    .sidenav li:hover{
-        background-color: #FFBF00 !important;
-    }
-    .sidenav li:active{
+
+    .sidenav li:hover {
         background-color: #FFBF00 !important;
     }
 
-    .admin .logo-wrapper .hide-on-med-and-down{
+    .sidenav li:active {
+        background-color: #FFBF00 !important;
+    }
+
+    .admin .logo-wrapper .hide-on-med-and-down {
         height: auto !important;
         width: 70px !important;
     }
-    .admin .logo-wrapper .show-on-medium-and-down{
+
+    .admin .logo-wrapper .show-on-medium-and-down {
         height: auto !important;
         width: 217px !important;
         margin-left: 176px !important;
         padding-top: 1px;
     }
+
     .btn-floating i {
         font-size: 1.6rem !important;
         line-height: 40px !important;
@@ -46,32 +47,31 @@
         background-color: #ec8100 !important;
     }
 
-    span.badge{
+    span.badge {
         min-width: 2rem !important;
-}
+    }
+
     .nav-collapsed.sidenav-main {
         width: 79px !important;
     }
 
-.vandor .vendor-img{
-    height: 46px !important ;
-    width:  auto !important;
-}
-.vandor .show-on-medium-and-down{
-    width: auto !important;
-    height: 41px !important;
-    margin-left: 32px !important;
-}
+    .vandor .vendor-img {
+        height: 46px !important;
+        width: auto !important;
+    }
 
-  #alternate-logo .hide-on-med-and-down  {
+    .vandor .show-on-medium-and-down {
+        width: auto !important;
+        height: 41px !important;
+        margin-left: 32px !important;
+    }
+
+    #alternate-logo .hide-on-med-and-down {
         height: 41px !important;
         padding-top: 5px !important;
     }
-.dataTables_scrollBody{
-    height: 552px !important;
 
-}
-
-
-
+    .dataTables_scrollBody {
+        min-height: 180px; 
+    }
 </style>
